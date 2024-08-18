9f9fc3bc56e6ceed29535c29946c2a4ad3d69a43
kimia <kimilonix22@gmail.com>
2024-08-18 21:35:32 +0330

    weather_api.py

--- weather_api.py
+++ weather_api.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-api_key = "c8b22766f37fa2baef794f849ce79bc8"
+api_key = "YOUR_API_KEY"
 city_name = input("Enter city name : ")
 
 complete_url = f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}"
